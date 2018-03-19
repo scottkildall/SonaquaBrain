0f0349a7697430d9e06861e5d7b9462ce056b901
Scott Kildall <scottkildall@Scotts-MacBook-Pro.local>
2018-03-18 23:03:06 -0700

    added autoplay mode

--- SonaquaBrain.ino
+++ SonaquaBrain.ino
@@ -41,7 +41,10 @@ Adafruit_7segment matrix = Adafruit_7segment();
 //-- capacitance touch sensor
 Adafruit_MPR121 cap = Adafruit_MPR121();
 
-#define LED_PIN (13)
+
+#define VOutPin (12)
+#define autoPlaySwitchPin (11)
+#define GndPin (10)
 
 //-- these are the output pins 2-13, which will correspond to the touch sensor
 //#define startPinOut (2)
@@ -55,33 +58,10 @@ const int startDOUTGroundPin = 30;         // these will be numbered 30, 32, 34,
 #define numSensors (12)
 
 MSTimer playTimers[numSensors] = MSTimer();
+boolean isPlaying[numSensors];
 
 void setup() {
-  //-- initalize digital pins 2-13 for output
-  /*
-   for( int i = 0; i < numSensors; i++ ) {
-    pinMode(startPinOut + i,OUTPUT);
-  }
-  */
 
-/*
-  for( int i = 0; i < numSensors; i++ ) {
-    digitalWrite(startPinOut + i,HIGH);
-     delay(100);
-    digitalWrite(startPinOut + i,LOW);
-    delay(100);
-  }
- */
- 
-  //-- led pin (are we going to use this?)
-  pinMode(LED_PIN,OUTPUT);
-
-  for( int i = 0; i < 4;i ++ ) {
-    digitalWrite(LED_PIN,HIGH);
-    delay(100);
-    digitalWrite(LED_PIN,LOW);
-    delay(100);
-  }
 
 //-- Digital out pins
   // All ground pins
@@ -96,13 +76,21 @@ void setup() {
     digitalWrite(startDOUTPositivePin, LOW);
   }
 
-//-- TESTER for pins 12 always OUT
-   pinMode(12, OUTPUT);
-   digitalWrite(12, HIGH);
-  
+   //-- for autoplay
+   pinMode(VOutPin, OUTPUT);
+   digitalWrite(VOutPin, HIGH);
+   pinMode(GndPin, INPUT);
+   digitalWrite(GndPin, LOW);
+   pinMode(autoPlaySwitchPin, INPUT);
+   
+   
   Serial.begin(115200);
   Serial.println("starting up");
-  
+
+  //-- isPlaying = false for all
+  for( int i; i < numSensors; i++ ) 
+    isPlaying[i] = false;
+    
   //-- initalize the 7-segment LED
   matrix.begin(0x70);
   matrix.print(9999, DEC);
@@ -127,18 +115,45 @@ void setup() {
 
 //-- get the touched status, which is a bitmask, i.e. pin 0 = 1 (if touched), pin 1 = 2 =
 void loop() {
+  checkAutoPlaySwitch();
+  
   if( bAutoPlay )
     autoPlayMode();
   else
     interactiveMode();
 }
 
+
+//-- this is a digital in at pin 11, 5V is on pin 12, so bridge the two with jumpers
+void checkAutoPlaySwitch() {
+  //-- do debounce etc
+  if( digitalRead(autoPlaySwitchPin) == true )
+    bAutoPlay = true;
+  else
+    bAutoPlay = false;
+
+  if( bAutoPlay ) 
+    Serial.println("autoplay");
+  else
+    Serial.println("interactive");  
+}
+
 void autoPlayMode() {
   for( int i = 0; i < numSensors; i++ ) {
-//    if( playTimers[i].isExpired() ) {
-//      
-//      
-//    }
+    // Check to see if we are playing and the playTimer is expired, if so, turn off
+    if( isPlaying[i] == false ) {
+      //--
+      if( random(100000) < 5) {
+        digitalWrite(startDOUTPositivePin + (i*2),HIGH);
+        playTimers[i].setTimer( random(500, 5000) );
+        isPlaying[i] = true;
+      }
+      
+    }
+    else if( isPlaying[i] == true && playTimers[i].isExpired() ) {
+        digitalWrite(startDOUTPositivePin + (i*2),LOW);
+        isPlaying[i] = false;
+    }
   }
 }
 
@@ -152,50 +167,21 @@ void interactiveMode() {
 //
 //  // delay(250);
 
- //-- NOT SURE WHY BUT BITMASK
- /*
-  unsigned short bitShift = 1;
-  Serial.println("-------------");
-   Serial.println(capBitmask);
-   //Serial.println( capBitmask & (1) );
-   //Serial.println( capBitmask & (1 >> 0) );
-    Serial.println(  (1 >> 0) );
-     
-   //Serial.println( capBitmask & (2) );
-   //Serial.println( capBitmask & (1 >> 1) );
-   bitShift = (1 >> 1);
-   Serial.println( bitShift );
-   //Serial.println( capBitmask & (4) );
-   //Serial.println( capBitmask & (1>>2) );
-    bitShift = (1 >> 2);
-   Serial.println(  bitShift  );
-      Serial.println("-------------");
-*/
-
+ 
+/*
 Serial.print("cap mask: " );
 Serial.println(capBitmask);
-
+*/
     
   //-- do cap-filtering here
    for(int i = 0; i < numSensors; i++ ) {
       //-- bitshift operators are not working with Arduino, so we are fudging it
       uint16_t bitMask = 0;
-     /* if( i == 0 )
-        bitMask = 1;
-      else {
-       */
-        
-      //}
-       
-       
-       //bitMask = pow(2, i);
-
+    
+      //-- bitmask hack because >> isn't working
       bitMask = 1;
       for( int j = 0; j < i; j++ )
         bitMask = bitMask * 2;
-          
-   //       Serial.print("bitMask: " );
-   //     Serial.println(bitMask);
       
       for( int j = 0; j < NUM_FILTERED_SAMPLES; j++ ) {
         if( capBitmask & bitMask ) 
